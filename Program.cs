464e4fc34e3da3e63a9e81a4f84f8ccd02a1cb20
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2023-09-18 11:10:29 +0300

    added loop feature

--- Program.cs
+++ Program.cs
@@ -1,45 +1,50 @@
-﻿Console.WriteLine("From which to which number do you wish to see the prime numbers?");
-Console.WriteLine("From:");
-int limit1 = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("To:");
-int limit2 = Convert.ToInt32(Console.ReadLine());
-int counter_1 = 2;
-int counter_2 = 0;
-while ((limit1 < 11) && (limit1 <= limit2))
+﻿while (true)
 {
-    while ((limit1 / 2) >= counter_1)
+    Console.WriteLine("From which to which number do you wish to see the prime numbers?");
+    Console.Write("From:");
+    int limit1 = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("To:");
+    int limit2 = Convert.ToInt32(Console.ReadLine());
+    int counter_1 = 2;
+    int counter_2 = 0;
+    while ((limit1 < 11) && (limit1 <= limit2))
     {
-        if ((limit1 % counter_1) == 0)
+        while ((limit1 / 2) >= counter_1)
         {
-            counter_2 += 1;
-            break;
-        }
-        counter_1 += 1;
-    }    
-    if (counter_2 == 0)
-    {
-        Console.WriteLine(limit1);
-    }    
-    limit1 += 1;
-    counter_1 = 2;
-    counter_2 = 0;
-}
-while ((limit1 >= 11) && (limit1 <= limit2))
-{
-    while ((limit1 / 4) >= counter_1)
-    {
-        if ((limit1 % counter_1) == 0)
+            if ((limit1 % counter_1) == 0)
+            {
+                counter_2 += 1;
+                break;
+            }
+            counter_1 += 1;
+        }    
+        if (counter_2 == 0)
         {
-            counter_2 += 1;
-            break;
-        }
-        counter_1 += 1;
-    }    
-    if (counter_2 == 0)
+            Console.WriteLine(limit1);
+        }    
+        limit1 += 1;
+        counter_1 = 2;
+        counter_2 = 0;
+    }
+    while ((limit1 >= 11) && (limit1 <= limit2))
     {
-        Console.WriteLine(limit1);
-    }    
-    limit1 += 1;
-    counter_1 = 2;
-    counter_2 = 0;
+        while ((limit1 / 4) >= counter_1)
+        {
+            if ((limit1 % counter_1) == 0)
+            {
+                counter_2 += 1;
+                break;
+            }
+            counter_1 += 1;
+        }    
+        if (counter_2 == 0)
+        {
+            Console.WriteLine(limit1);
+        }    
+        limit1 += 1;
+        counter_1 = 2;
+        counter_2 = 0;
+    }
+    Console.Write("press enter to use the program again");
+    Console.ReadLine();
 }
